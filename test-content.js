6c0b7e976f1927cc24dd7c230ee6c59579cc8f25
LemonTheAxolotl <yunzhongwu@gmail.com>
2026-09-05 10:34:39 -0400

    Delete test-content.js

--- test-content.js
+++
@@ -1,7 +0,0 @@
-// Node-only bootstrap used by automated tests; browsers use data-loader.js.
-const fs=require('node:fs');
-const path=require('node:path');
-const schema=require('./content-schema.js');
-const names=['maps','orbs','towers','achievements','tutorial','waves','difficulties'];
-const raw=Object.fromEntries(names.map(name=>[name,JSON.parse(fs.readFileSync(path.join(__dirname,'data',`${name}.json`),'utf8'))]));
-module.exports=schema.build(raw);
